d4d968c0d35c43594a4740b48f32283549e1d2c8
Tim Abell <tim@timwise.co.uk>
2024-08-02 17:30:55 +0100

    Update to match blog

--- CollectionFixtureExampleTests.cs
+++ CollectionFixtureExampleTests.cs
@@ -1,8 +1,13 @@
+// https://timwise.co.uk/2024/08/02/running-xunit-test-setup-only-once/
+
+/// <summary>
+/// Setup / teardown code that the tests need to work.
+/// </summary>
 public class SharedFixture : IDisposable
 {
     public SharedFixture()
     {
-        Console.WriteLine($"Running {nameof(SharedFixture)} constructor -  Setup code that runs once across all test classes");
+        Console.WriteLine($"Running {nameof(SharedFixture)} constructor -  Setup code that runs once across all test classes.");
     }
 
     public void Dispose()
@@ -13,43 +18,45 @@ public class SharedFixture : IDisposable
 
 /// <summary>
 /// This exists to prevent xUnit running the SharedFixture setup more than once
-/// concurrently when multiple test classes are executed at once.
+/// concurrently when multiple test classes are executed at once,
+/// and as a middle point in connecting the fixture to the test classes.
 /// </summary>
-[CollectionDefinition(nameof(SharedFixture))]
+[CollectionDefinition(nameof(SharedFixtureCollection))]
 public class SharedFixtureCollection : ICollectionFixture<SharedFixture> { }
 
-[Collection(nameof(SharedFixture))]
+[Collection(nameof(SharedFixtureCollection))]
 public class TestClass1 : IClassFixture<SharedFixture>
 {
     SharedFixture _fixture;
 
     public TestClass1(SharedFixture fixture)
     {
-        _fixture = fixture;
-    }
-
-    [Fact]
-    public void Test1()
-    {
-        Console.Out.WriteLine($"Running {nameof(TestClass1)}.{nameof(TestClass1.Test1)}");
-        Assert.True(true);
-    }
-}
-
-[Collection(nameof(SharedFixture))]
-public class TestClass2 : IClassFixture<SharedFixture>
-{
-    SharedFixture _fixture;
-
-    public TestClass2(SharedFixture fixture)
-    {
-        _fixture = fixture;
+        _fixture = fixture; // this is how you get access to the fixture from the tests
     }
 
     [Fact]
     public void Test2()
     {
-        Console.Out.WriteLine($"Running {nameof(TestClass2)}.{nameof(TestClass2.Test2)}");
+        Console.Out.WriteLine($"- Running {nameof(TestClass1)}.{nameof(TestClass1.Test2)}");
+        //_fixture.DoSomething();
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        Console.Out.WriteLine($"- Running {nameof(TestClass1)}.{nameof(TestClass1.Test1)}");
+        Assert.True(true);
+    }
+}
+
+[Collection(nameof(SharedFixtureCollection))]
+public class TestClass2 : IClassFixture<SharedFixture>
+{
+    [Fact]
+    public void Test3()
+    {
+        Console.Out.WriteLine($"- Running {nameof(TestClass2)}.{nameof(TestClass2.Test3)}");
         Assert.True(true);
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+Test/example code for <https://timwise.co.uk/2024/08/02/running-xunit-test-setup-only-once/>
