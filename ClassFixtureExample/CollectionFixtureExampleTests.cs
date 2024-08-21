b2e6f0b6f377ef552d8b44bdcbc66109a32a8bf2
Tim Abell <tim@timwise.co.uk>
2024-08-21 19:09:54 +0100

    Add fixture count to show instance re-use

--- ClassFixtureExample/CollectionFixtureExampleTests.cs
+++ ClassFixtureExample/CollectionFixtureExampleTests.cs
@@ -5,6 +5,8 @@
 /// </summary>
 public class SharedFixture : IDisposable
 {
+    public int CallCount { get; set; }
+
     public SharedFixture()
     {
         Console.WriteLine($"Running class-fixture {nameof(SharedFixture)} constructor -  Setup code that runs once across all test classes.");
@@ -12,7 +14,7 @@ public class SharedFixture : IDisposable
 
     public void Dispose()
     {
-        Console.WriteLine($"Running class-fixture {nameof(SharedFixture)} dispose -  Cleanup code that runs once after all tests are done");
+        Console.WriteLine($"Running class-fixture {nameof(SharedFixture)} dispose -  Cleanup code that runs once after all tests are done. {CallCount} calls made to this fixture instance");
     }
 }
 
@@ -32,6 +34,7 @@ public class TestClass1 : IClassFixture<SharedFixture>
         Console.Out.WriteLine($"- Running class-fixture {nameof(TestClass1)}.{nameof(TestClass1.Test2)}");
         //_fixture.DoSomething();
         Assert.True(true);
+        _fixture.CallCount++;
     }
 
     [Fact]
@@ -39,15 +42,25 @@ public class TestClass1 : IClassFixture<SharedFixture>
     {
         Console.Out.WriteLine($"- Running class-fixture {nameof(TestClass1)}.{nameof(TestClass1.Test1)}");
         Assert.True(true);
+        _fixture.CallCount++;
     }
 }
 
 public class TestClass2 : IClassFixture<SharedFixture>
 {
+    SharedFixture _fixture;
+
+    public TestClass2(SharedFixture fixture)
+    {
+        Console.WriteLine($"- Running class-fixture {nameof(TestClass2)} constructor");
+        _fixture = fixture; // this is how you get access to the fixture from the tests
+    }
+
     [Fact]
     public void Test3()
     {
         Console.Out.WriteLine($"- Running class-fixture {nameof(TestClass2)}.{nameof(TestClass2.Test3)}");
         Assert.True(true);
+        _fixture.CallCount++;
     }
 }
